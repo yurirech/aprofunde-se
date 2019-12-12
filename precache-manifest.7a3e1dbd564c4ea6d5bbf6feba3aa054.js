3db03100d3af7a29534424e96014d0ab15b3c64b
Yuri Nunes <yuri.nunes@tuntscorp.com>
2019-12-12 19:32:32 +0100

    Updates

---
+++ precache-manifest.7a3e1dbd564c4ea6d5bbf6feba3aa054.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714ba8f9832085cf653236b83ed65974",
+    "url": "/aprofunde-se/index.html"
+  },
+  {
+    "revision": "524768d352e6422e4b37",
+    "url": "/aprofunde-se/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f40212afe07c6e6ef4b3",
+    "url": "/aprofunde-se/static/css/main.69066c75.chunk.css"
+  },
+  {
+    "revision": "524768d352e6422e4b37",
+    "url": "/aprofunde-se/static/js/2.b2e859cf.chunk.js"
+  },
+  {
+    "revision": "f40212afe07c6e6ef4b3",
+    "url": "/aprofunde-se/static/js/main.5ff17b91.chunk.js"
+  },
+  {
+    "revision": "7c48acd76ddb571e76b1",
+    "url": "/aprofunde-se/static/js/runtime-main.301e9a01.js"
+  },
+  {
+    "revision": "85154f6f051e8a4dce5ecd081070c356",
+    "url": "/aprofunde-se/static/media/adventure.85154f6f.jpg"
+  },
+  {
+    "revision": "a3c3f49731d50929505808737429df2a",
+    "url": "/aprofunde-se/static/media/arrow-left.a3c3f497.png"
+  },
+  {
+    "revision": "03b575462922cf95b2d098650e06e319",
+    "url": "/aprofunde-se/static/media/arrow-right.03b57546.png"
+  },
+  {
+    "revision": "4e13ecd3d60ed1529b88644465bfd0b0",
+    "url": "/aprofunde-se/static/media/culture.4e13ecd3.jpg"
+  },
+  {
+    "revision": "184266855d8ad101303f0b934aee8944",
+    "url": "/aprofunde-se/static/media/header-map.18426685.jpg"
+  },
+  {
+    "revision": "efee53596dd9ed383637ca076d69de3d",
+    "url": "/aprofunde-se/static/media/housesTour.efee5359.jpg"
+  },
+  {
+    "revision": "36e3dc04b7f4aa46e72bb7c5423ef3fb",
+    "url": "/aprofunde-se/static/media/millsTour.36e3dc04.jpg"
+  },
+  {
+    "revision": "68c4db8653b7177cea8e23bbc0f469d4",
+    "url": "/aprofunde-se/static/media/nature.68c4db86.jpg"
+  },
+  {
+    "revision": "a99d5beeeff2333f1036b0a2a2b7b32c",
+    "url": "/aprofunde-se/static/media/new-header.a99d5bee.jpg"
+  },
+  {
+    "revision": "d8f68b99f023adaf70abc979034ed509",
+    "url": "/aprofunde-se/static/media/relaxe.d8f68b99.jpg"
+  },
+  {
+    "revision": "e624a3392b02639ddf87a3aeabf557f1",
+    "url": "/aprofunde-se/static/media/technology.e624a339.jpg"
+  }
+]);
