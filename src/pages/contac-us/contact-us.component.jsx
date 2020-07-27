a122c2de31359b23a977a3c07d28f93b725f5d0a
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-07-27 20:36:32 +0200

    page finished for desktop

--- src/pages/contac-us/contact-us.component.jsx
+++ src/pages/contac-us/contact-us.component.jsx
@@ -7,36 +7,43 @@ import {dayTours, packagesTours, accomodation} from "./contact-us.data";
 
 import InputText from "../../components/shared-components/input-text/input-text.component";
 import RadioCheckbox from "../../components/shared-components/radio-checkbox/radio-checkbox.component";
+import styled from "styled-components";
+
+const SectionTitle = styled.h5`
+  margin: 2.05rem 0 1rem 0;
+  color: ${colors.aprofundeSeOrange};
+`;
 
 const ContactUs = () => (
   <Content>
-    <FlexContainer column margin='4rem auto' width='50%'>
+    <FlexContainer column margin='4rem auto' width='50%' widthMd='80%'>
       <h1 style={{fontSize: '4rem', color: colors.aprofundeSeOrange}}>Entre em contato</h1>
     <form>
       <InputText type='text' controlId='name' placeholder='Nome' />
       <InputText type='email' controlId='email' placeholder='Email' />
-      <h5>Day Tours</h5>
+      <SectionTitle>Day Tours</SectionTitle>
       {
         dayTours.map((title, i) => (
-
               <RadioCheckbox key={i} type='checkbox' id={`day-tours-${i}`} label={title} name={title} />
             )
          )
       }
-      <h5>Pacotes</h5>
+      <SectionTitle>Pacotes</SectionTitle>
       {
         packagesTours.map((title, i) => (
-              <RadioCheckbox key={i} type='radio' id={`package-${i}`} label={title} name={title} />
+              <RadioCheckbox key={i} type='radio' id={`package-${i}`} label={title} name='packages' />
             )
          )
       }
-      <h5>Hospedagem</h5>
-      {
-        accomodation.map((title, i) => (
+      <div style={{marginBottom: '2rem'}}>
+        <SectionTitle last>Hospedagem</SectionTitle>
+        {
+          accomodation.map((title, i) => (
               <RadioCheckbox key={i} type='checkbox' id={`accommodation-${i}`} label={title} name={title} />
             )
-         )
-      }
+          )
+        }
+      </div>
       <InputText textArea='textarea' type='text' controlId='numberOfPassengers'
                  placeholder='Nos fale mais como podemos te ajudar' />
       <InputText type='text' controlId='numberOfPassengers'
