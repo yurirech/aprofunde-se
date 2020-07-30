fed5573d36198eb7974640571408fc12c3803d3c
Yuri Rech <35239431+yurirech@users.noreply.github.com>
2020-07-30 10:47:02 +0200

    Contact us (#32)
    * create contact us form
    
    * started adding css to contact us
    
    * merge
    
    * page finished for desktop
    
    * make input works
    
    Co-authored-by: Yuri Rech <yuri.nunes@tuntscorp.com>

--- src/components/shared-components/input-text/input-text.component.jsx
+++ src/components/shared-components/input-text/input-text.component.jsx
@@ -6,9 +6,11 @@ const FormGroup = styled(Form.Group)`
   margin: 1rem 0;
   `;
 
-const InputText = ({controlId, type, placeholder, textArea}) =>  (
+const InputText = ({controlId, type, placeholder, name, required, value, onChange, textArea}) =>  (
       <FormGroup controlId={controlId}>
-        <Form.Control as={textArea} type={type} rows={textArea === 'textarea' ? 3 : null} placeholder={placeholder} />
+        <Form.Control required={!!required} as={textArea} type={type}
+                      rows={textArea === 'textarea' ? 3 : null} defaultValue={value}
+                      name={name} placeholder={placeholder} onChange={onChange} />
       </FormGroup>
     );
 
