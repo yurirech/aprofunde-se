27d4f2ebac290081a9a28360ae961bd47a3fdd87
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-08-21 18:56:27 +0200

    finished contact form

--- src/pages/contac-us/contact-us.component.jsx
+++ src/pages/contac-us/contact-us.component.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import {Content, FormButton, HeaderTitle, SectionTitle} from "./contact-us.styles";
 import {FlexContainer} from "../../_styles";
-import {dayTours, packagesTours, accomodation} from "./contact-us.data";
+import {dayTours, packagesTours, accommodation} from "./contact-us.data";
+
 
 import InputText from "../../components/shared-components/input-text/input-text.component";
 import RadioCheckbox from "../../components/shared-components/radio-checkbox/radio-checkbox.component";
@@ -11,38 +12,43 @@ import RadioCheckbox from "../../components/shared-components/radio-checkbox/rad
 
 const ContactUs = () => {
   const [name, setName] = useState('');
-  // const [email, setEmail] = useState('lalalala');
-  // const firstRender = useRef(true);
-  // const [disabled, setDisabled] = useState(true);
-  // const [nameError, setNameError] = useState(null)
+  const [email, setEmail] = useState('');
+  const [moreInfo, setMoreInfo] = useState('');
+  const [packages, setPackages] = useState('');
+  const [numberOfTravelers, setNumberOfTravelers] = useState('');
+  const [dayToursGroup, setDayToursGroup] = useState([]);
+  const [accommodations, setAccommodations] = useState([]);
 
-  // useEffect(() => {
-  //   if (firstRender.current) {
-  //     firstRender.current = false  // it's no longer the first render
-  //     return; // skip the code below
-  //   }
-  //
-  //   setDisabled( handleValidation() );
-  //
-  //   }, [name, email])
-
-  // const handleValidation = () => {
-  // if (name === "") {
-  //   setNameError('Name can\'t be blank!');
-  //   return true;
-  // } else {
-  //   setNameError(null)
-  //   return false;
-  // }
-  // }
-
-  const handleChange = (e) => {
-
+  useEffect(
+  () => {
+    console.log(packages);
   }
+  )
+
+  const handleDayTours = e => {
+  if(e.target.checked && e.target.name === 'dayTours') {
+    setDayToursGroup([...dayToursGroup, e.target.value]);
+  } else {
+    setDayToursGroup(dayToursGroup.filter(dayTour => dayTour !== e.target.value))
+  }
+}
+
+const handleAccommodation = e => {
+  if(e.target.checked && e.target.name === 'accommodations') {
+    setAccommodations([...accommodations, e.target.value]);
+  } else {
+    setAccommodations(accommodations.filter(accommodation => accommodation !== e.target.value))
+  }
+}
+
 
   const handleSubmit = e => {
     e.preventDefault();
-    alert(`Submitting Name ${name}`)
+    setEmail(e.target.email.value);
+    setName(e.target.name.value);
+    setMoreInfo(e.target.moreInfo.value);
+    setPackages(e.target.packages.value);
+    setNumberOfTravelers(e.target.numberOfTravelers.value);
   }
 
   return (
@@ -50,38 +56,87 @@ const ContactUs = () => {
       <FlexContainer column margin='4rem auto' width='50%' widthMd='80%'>
         <HeaderTitle>Entre em contato</HeaderTitle>
         <form onSubmit={handleSubmit}>
-          <InputText type='text' placeholder='Nome' value={name} onChange={ e => {
-            console.log(e.target.value);
-            return e.target.value
-          }}  />
-          <InputText required type='email' controlId='email' placeholder='Email' />
+          <InputText type='text'
+                     placeholder='Nome'
+                     value={name}
+                     name='name'
+                     onChange={ e => e.target.value }
+          />
+
+          <InputText required
+                     type='email'
+                     value={email}
+                     name='email'
+                     onChange={ e => e.target.value }
+                     placeholder='Email'
+          />
+
           <SectionTitle>Day Tours</SectionTitle>
-          {
-            dayTours.map((title, i) => (
-                <RadioCheckbox key={i} type='checkbox' id={`day-tours-${i}`} label={title} name={title} />
+
+            {
+              dayTours.map((title, i) => (
+                  <RadioCheckbox key={i}
+                                 type='checkbox'
+                                 id={`day-tours-${i}`}
+                                 label={title}
+                                 value={title}
+                                 name='dayTours'
+                                 onChange={ handleDayTours }
+                  />
+                )
               )
-            )
-          }
+            }
+
           <SectionTitle>Pacotes</SectionTitle>
+
           {
             packagesTours.map((title, i) => (
-                <RadioCheckbox key={i} type='radio' id={`package-${i}`} label={title} name='packages' />
+                <RadioCheckbox key={i}
+                               type='radio'
+                               id={`package-${i}`}
+                               onChange={ e => e.target.value }
+                               label={title}
+                               value={title}
+                               name='packages'
+                />
               )
             )
           }
+
           <div style={{marginBottom: '2rem'}}>
+
             <SectionTitle last>Hospedagem</SectionTitle>
+
             {
-              accomodation.map((title, i) => (
-                  <RadioCheckbox key={i} type='checkbox' id={`accommodation-${i}`} label={title} name={title} />
+              accommodation.map((title, i) => (
+                  <RadioCheckbox
+                    key={i}
+                    type='checkbox'
+                    id={`accommodation-${i}`}
+                    label={title}
+                    value={packages}
+                    name='accommodations'
+                    onChange={ handleAccommodation }
+                  />
                 )
               )
             }
           </div>
-          <InputText textArea='textarea' type='text' controlId='numberOfPassengers'
-                     placeholder='Nos fale mais como podemos te ajudar' />
-          <InputText type='text' controlId='numberOfPassengers'
-                     placeholder='Quantos viajantes e quantos quartos' />
+          <InputText textArea='textarea'
+                     type='text'
+                     placeholder='Nos fale mais como podemos te ajudar'
+                     name='moreInfo'
+                     value={moreInfo}
+                     onChange={e => e.target.value}
+          />
+
+         <InputText type='text'
+                    name='numberOfTravelers'
+                    placeholder='Quantos viajantes e quantos quartos'
+                    value={numberOfTravelers}
+                    onChange={e => e.target.value}
+         />
+
           <FormButton type='submit'>Enviar</FormButton>
         </form>
       </FlexContainer>
