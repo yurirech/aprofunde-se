3b120f4242a9dbe278e47c18d5b02d755c79b891
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-08-25 15:33:32 +0200

    fix(header): fix many header features

--- src/components/shared-components/main-navbar/main-navbar.component.jsx
+++ src/components/shared-components/main-navbar/main-navbar.component.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import {Header} from "./main-navbar.styles";
+import React, {useState, useEffect} from 'react';
+import {Dropdown, Header} from "./main-navbar.styles";
 import {withRouter, Link} from 'react-router-dom'
 import {Nav, Navbar} from "react-bootstrap";
 import {ReactComponent as AprofundeSeLogo} from '../../../assets/icon/aprofunde-se-logo-navbar.svg';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const MainNavBar = ({location}) => {
+    const [openDayTours, setOpenDayTours] = useState(true);
+    const [openPackages, setOpenPackages] = useState(true);
+    const [openInspire, setOpenInspire] = useState(true);
+
+    useEffect(() => {
+      console.log(openDayTours);
+      console.log(openPackages);
+      console.log(openInspire);
+    })
+
     return (
-      <Header headerAbsolute={location.pathname === '/' ? 'absolute' : null} >
-        <Navbar>
+      <Header headerAbsolute={location.pathname === '/' ? 'absolute' : null}>
+        <Navbar collapseOnSelect>
           <div className='nav-header'>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Link to='/'>
@@ -18,42 +29,100 @@ const MainNavBar = ({location}) => {
           </div>
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link to='/'>Início</Link>
-            <Link to='/about'>Sobre</Link>
-            <div className='dropdown'>
-              <Link to='/day-tours'>Day Tours</Link>
-              <div className='dropdown-items'>
-                <Link to='/day-tours/gouda'>Gouda & Rotterdam</Link>
-                <Link to='/day-tours/haarlem'>Haarlem & Praia de bicicleta</Link>
-                <Link to='/day-tours/delft'>T.U. Delft & Fazenda</Link>
-                <Link to='/day-tours/giethoorn'>Giethoorn & Dolmen</Link>
-                <Link to='/day-tours/delta'>Delta Works & Middelburg</Link>
-                <Link to='/day-tours/utrecht'>Utrecht & Castelo</Link>
-                <Link to='/day-tours/haag'>Den Haag & Museu Voorlinden</Link>
-                <Link to='/day-tours/kroller'>Museu Kroller Müller & Amersfoort</Link>
-                <Link to='/day-tours/zaanse'>Zaanse Schans & Volendam</Link>
-                <Link to='/day-tours/park'>Parque de diversões & Sauna</Link>
+            <Nav.Link as={Link} eventKey='1' to='/'>Início</Nav.Link>
+            <Nav.Link as={Link} eventKey='2' to='/about'>Sobre</Nav.Link>
+
+            <Dropdown displayDropdown={openDayTours ? 'none' : null}>
+             <div className='header-dropdown'>
+               <Nav.Link as={Link} eventKey='3' to='/day-tours'>Day Tours</Nav.Link>
+               {
+                 openDayTours ?
+
+                   <FontAwesomeIcon
+                     onClick={() => setOpenDayTours(() => !openDayTours)}
+                     className='menu-icon-mobile'
+                     icon='plus-circle'
+                   />
+                   :
+                   <FontAwesomeIcon
+                     onClick={() => setOpenDayTours(() => !openDayTours)}
+                     className='menu-icon-mobile'
+                     icon='minus-circle'
+                   />
+               }
+             </div>
+
+              <div className='dropdown-items day-tours'>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/gouda'>Gouda & Rotterdam</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/haarlem'>Haarlem & Praia de bicicleta</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/delft'>T.U. Delft & Fazenda</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/giethoorn'>Giethoorn & Dolmen</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/delta'>Delta Works & Middelburg</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/utrecht'>Utrecht & Castelo</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/haag'>Den Haag & Museu Voorlinden</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/kroller'>Museu Kroller Müller & Amersfoort</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/zaanse'>Zaanse Schans & Volendam</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/day-tours/park'>Parque de diversões & Sauna</Nav.Link>
               </div>
-            </div>
-            <div className='dropdown'>
-              <Link to='/packages'>Pacotes</Link>
-              <div className='dropdown-items'>
-                <Link to='/packages/embrenha'>Embrenhe-se</Link>
-                <Link to='/packages/entranhe'>Entranhe-se</Link>
-                <Link to='/packages/entregue'>Entregue-se</Link>
+            </Dropdown>
+
+            <Dropdown displayDropdown={openPackages ? 'none' : null}>
+              <div className='header-dropdown'>
+                <Nav.Link as={Link} eventKey='2' to='/packages'>Pacotes</Nav.Link>
+
+                {
+                  openPackages ?
+
+                    <FontAwesomeIcon
+                      onClick={() => setOpenPackages(() => !openPackages)}
+                      className='menu-icon-mobile'
+                      icon='plus-circle'
+                    />
+                    :
+                    <FontAwesomeIcon
+                      onClick={() => setOpenPackages(() => !openPackages)}
+                      className='menu-icon-mobile'
+                      icon='minus-circle'
+                    />
+                }
               </div>
-            </div>
-            <div className='dropdown'>
-            <Link to='/inspire'>Inspire-se</Link>
+
               <div className='dropdown-items'>
-                <Link to='/inspire/innovate'>Inove-se</Link>
-                <Link to='/inspire/relax'>Relaxe-se</Link>
-                <Link to='/inspire/adventure'>Aventure-se</Link>
+                <Nav.Link as={Link} eventKey='2' to='/packages/embrenha'>Embrenhe-se</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/packages/entranhe'>Entranhe-se</Nav.Link>
+                <Nav.Link as={Link} eventKey='2' to='/packages/entregue'>Entregue-se</Nav.Link>
               </div>
-            </div>
+            </Dropdown>
+
+            <Dropdown displayDropdown={openInspire ? 'none' : null}>
+             <div className='header-dropdown'>
+               <Nav.Link as={Link} eventKey='2' to='/inspire'>Inspire-se</Nav.Link>
+               {
+                 openInspire ?
+
+                   <FontAwesomeIcon
+                     onClick={() => setOpenInspire(() => !openInspire)}
+                     className='menu-icon-mobile'
+                     icon='plus-circle'
+                   />
+                   :
+                   <FontAwesomeIcon
+                     onClick={() => setOpenInspire(() => !openInspire)}
+                     className='menu-icon-mobile'
+                     icon='minus-circle'
+                   />
+
+               }
+             </div>
+                <div className='dropdown-items'>
+                  <Nav.Link as={Link} eventKey='2' to='/inspire/innovate'>Inove-se</Nav.Link>
+                  <Nav.Link as={Link} eventKey='2' to='/inspire/relax'>Relaxe-se</Nav.Link>
+                  <Nav.Link as={Link} eventKey='2' to='/inspire/adventure'>Aventure-se</Nav.Link>
+                </div>
+            </Dropdown>
            
-            <Link to='/instagram'>Instagram</Link>
-            <Link to='/contact-us'>Contato</Link>
+            <Nav.Link as={Link} eventKey='2' to='/instagram'>Instagram</Nav.Link>
+            <Nav.Link as={Link} eventKey='2' to='/contact-us'>Contato</Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Navbar>
--- src/components/shared-components/main-navbar/main-navbar.styles.js
+++ src/components/shared-components/main-navbar/main-navbar.styles.js
@@ -17,57 +17,19 @@ export const Header = styled.div`
 
   .navbar-nav {
     align-items: end;
-    margin-top: 1rem;
+    
     a {
-      padding: .25rem .75rem;
-      color: ${colors.aprofundeSeBlackText};
+      padding: .25rem .5rem;
+      color: ${colors.aprofundeSeBlackText} !important;
       font-size: .95rem;
     }
   }
   
-  /* Dropdown menu */
-.dropdown {
-  position: relative;
-  display: inline-block;
-  padding: .25rem .75rem;
-
-  a {
-    padding: unset;
+  .menu-icon-mobile {
+    display: none;
   }
   
-  &:hover {
-    .dropdown-items {
-      display: block;
-      animation: load .5s forwards;
-    }
-  }
-
-  .dropdown-items {
-      display: none;
-      position: absolute;
-      background-color: #f1f1f1;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-      opacity: 0;
-      
-      
-      a {
-      color: black;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-    }
-  }
-  @keyframes load {
-  100% {
-    opacity: 1;
-  }
-}
-}
-
-
-  /* Dropdown menu ends */
+  
  
  @media ${device.tablet} {
  .nav-header {
@@ -83,7 +45,12 @@ export const Header = styled.div`
   .navbar-brand {
    display: block;
    margin-right: auto;
+   
   }    
+ }
+ 
+ .navbar-nav {
+    margin-top: 1rem;
  }
  
  .navbar-collapse {
@@ -104,6 +71,13 @@ export const Header = styled.div`
    flex-direction: column; 
 }
 
+.menu-icon-mobile {
+  display: inline-block;
+  margin-left: .25rem;
+  color: ${colors.aprofundeSeOrange};
+  font-size: .85rem;
+}
+
   .collapse:not(.show) {
       display: none!important;
   }
@@ -111,6 +85,8 @@ export const Header = styled.div`
      display: block;
     }
   } 
+  
+}
 
   @media ${device.mobileL} {
     background-color: #ffffff;
@@ -123,3 +99,101 @@ export const Header = styled.div`
     }
   }
 `;
+
+export const Dropdown = styled.div`
+ /* Dropdown menu */
+  position: relative;
+  display: inline-block;
+  
+  a {
+    padding: unset;
+  }
+  
+  &:hover {
+    .dropdown-items {
+      display: block;
+      animation: load .5s forwards;
+    }
+  }
+
+  .dropdown-items {
+      display: none;
+      position: absolute;
+      background-color: #fff;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+      opacity: 0;
+      
+      
+      a {
+      color: black;
+      padding: .75rem 1rem;
+      text-decoration: none;
+      display: block;
+      transition: background-color .2s;
+      
+      &:hover {
+        background-color: ${colors.aprofundeSeOrange};
+        color: #fff4ed !important;
+      }
+    }
+  }
+  
+  .day-tours {
+    min-width: 13.75rem;
+  }
+  
+  @keyframes load {
+  100% {
+    opacity: 1;
+  }
+}
+  /* Dropdown menu ends */
+  
+  @media ${device.tablet} {
+     /* Dropdown menu */
+     padding: .2rem 0;
+     
+  &:hover {
+      .dropdown-items {
+        display: none;
+        animation: unset;
+      }
+    }
+    
+    .header-dropdown {
+      display: flex;
+      align-items: center;
+      
+    }
+    
+    .dropdown-items {
+        display: ${({displayDropdown}) => displayDropdown || 'unset' } !important;
+        position: unset;
+        box-shadow: unset;
+        
+        a {
+          padding: .5rem 1rem;
+          position: relative;
+          margin-left: .75rem;
+          
+           &:hover {
+          background-color: unset;
+          color: unset !important;
+          }
+          
+          &::before {
+            content: '';
+            position: absolute;
+            width: 5px;
+            height: 5px;
+            background: ${colors.aprofundeSeOrange}; 
+            top: 17px;
+            left: -3px;
+            border-radius: 50%;
+          }
+        }
+    }
+  }
+`
